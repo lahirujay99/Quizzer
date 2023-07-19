bb788ada4a1a7db2ac1e33f524d05a6780907678
lahirujay99 <lahirujayawardana94@gmail.com>
2023-07-19 14:42:15 +0530

    implemented useReducer, reducer func, initial state, pass data to reducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  // loading error ready active finish
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("unknown type");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // useEffect(function () {
+  //   async function getData() {
+  //     const res = await fetch("http://localhost:8000/questions");
+  //     const data = res.json();
+  //     console.log(data);
+  //   }
+  //   getData();
+  // }, []);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
