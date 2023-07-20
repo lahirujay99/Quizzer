833cef668d3df29f3a628268790160e7d2005819
lahirujay99 <lahirujayawardana94@gmail.com>
2023-07-20 23:37:20 +0530

    completed timer logic

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
 
+const SECS_PER_QUESTION = 30;
+
 const initialState = {
   questions: [],
   // status = loading, error, ready, active, finish,
@@ -19,6 +21,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -28,7 +31,11 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "startQuestion":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
     case "newAnswer":
       const question = state.questions.at(state.index);
       return {
@@ -49,16 +56,24 @@ function reducer(state, action) {
           state.points > state.highscore ? state.points : state.highscore,
       };
     case "restart":
-      // return { ...initialState, questions: state.questions , status:'ready'};
-      return { ...state, status: "ready", index: 0, answer: null, points: 0 };
+      return { ...initialState, questions: state.questions, status: "ready" };
+    // return { ...state, status: "ready", index: 0, answer: null, points: 0 };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("unknown type");
   }
 }
 
 function App() {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] =
-    useReducer(reducer, initialState);
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const qNum = questions.length;
   const maxPossiblePoints = questions.reduce(
@@ -102,7 +117,7 @@ function App() {
               answer={answer}
             />
             <Footer>
-              <Timer />
+              <Timer dispatch={dispatch} timeR={secondsRemaining} />
               <NextButton
                 dispatch={dispatch}
                 answer={answer}
